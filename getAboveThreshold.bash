4402c974ad7d5ca103f98250d3e2e90c5188eb8c
WhenGryphonsFly <84215159+WhenGryphonsFly@users.noreply.github.com>
2023-03-31 10:29:52 -0500

    Update getAboveThreshold.bash
    Prevents a warning from appearing on personal computers and other systems that do not use environment modules. In any case, going by the example in README.md, the modules should already be loaded by the time this is called by the preprocessor.

--- getAboveThreshold.bash
+++ getAboveThreshold.bash
@@ -6,8 +6,8 @@ cutoff=$3
 out=$4
 thread=$5
 
-module load samtools
-module load bedtools2
+#module load samtools
+#module load bedtools2
 
 samtools view -hb $inputFile $chr -@ $thread | bedtools genomecov -ibam - -pc -bga | grep -w "^$chr" | awk -v c=$cutoff '{if($4 > c){print}}' > $out
 
