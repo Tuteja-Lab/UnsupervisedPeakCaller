7f640341eb70582361b17436930e426797fa8461
Karin Dorman <kdorman@iastate.edu>
2023-11-21 23:39:22 -0600

    Reformatting and adapting to using new intermediate file naming conventions.

--- bigInputs.R
+++ bigInputs.R
@@ -1,34 +1,37 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
-# test if there is at least one argument: if not, return an error
-if (length(args)==0) {
-  stop("At least one argument must be supplied (input file).", call.=FALSE)
-} else if (length(args)==1) {
-  # default output file
-  args[2] = "out.txt"
+# test if there is at least four arguments: if not, return an error
+if (length(args) < 5) {
+	stop("At least two argument must be supplied.", call.=FALSE)
 }
 
-
+dir=args[1]
+chr.file=args[2]
+prefix=args[3]
+ref.prefix=args[4]
+out.file=args[5]
 
 library(dplyr)
 
 final <- data.frame(V1=NA, V2=NA, V3=NA, V4=NA)
-chr <- read.table(args[1], header = F)
+chr <- read.table(paste0(dir, "/", chr.file), header = F)
 
 for (i in chr$V1) {
-if (file.size(paste0(args[2], "/chr", i, "/", i, "regions2.txt")) > 0) {
-a <- read.table(paste0(args[2], "/chr", i, "/", i, "regions2.txt"), header = F)
-a1 <- sub("_.*", "", a$V4)
-b <- read.table(paste0(args[2], "/chr", i, "/", "temp", i, ".txt"), header = F)
-b$V4 <- paste0(b$V1, b$V4)
-b <- b[b$V4 %in% a1,]
-b <- b[,1:4]
-b <- distinct(b)
-#b$V4 <- paste0(b$V1, b$V4)
-final <- rbind(final, b)
-}
+	# there are 1000 bp regions outside blacklist regions
+	if (file.size(paste0(dir, "/", ref.prefix, i, "/", prefix, "_", i, "-regions4.txt")) > 0) {
+		a <- read.table(paste0(dir, "/", ref.prefix, i, "/", prefix, "_", i, "-regions4.txt"), header = F)
+		a1 <- sub("_.*", "", a$V4)
+		# these are original regions passing threshold
+		b <- read.table(paste0(dir, "/", ref.prefix, i, "/", prefix, "_", i, "-regions2.txt"), header = F)
+		b$V4 <- paste0(b$V1, b$V4)
+		b <- b[b$V4 %in% a1,]
+		b <- b[,1:4]
+		b <- distinct(b)
+		#b$V4 <- paste0(b$V1, b$V4)
+		final <- rbind(final, b)
+	}
 }
 final <- final[!is.na(final$V1),]
-write.table(final, paste0(args[2], "/bigInputs.txt"), quote = F, sep = "\t", row.names = F)
+write.table(final, out.file, quote = F, sep = "\t", row.names = F, col.names = F)
 
