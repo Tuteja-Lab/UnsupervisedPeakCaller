b136172e362155e97721bf97d9e2179ae05c7213
Karin Dorman <kdorman@iastate.edu>
2023-11-22 01:23:08 -0600

    Changing user interface to run_rcl.sh to match preprocessing.bash; allowing save and reuse of previous runs.

--- run_rcl.sh
+++ run_rcl.sh
@@ -1,29 +1,52 @@
 #!/bin/bash
+# Run RCL.
+# Input: PATH to data files, list of BAM file replicates BFILE1, BFILE2, ...
+# Assumes: PATH/chrList.txt, PATH/chr*/BFILE1.covBga.txt, PATH/chr*/BFILE2.covBga.txt, PATH/bigInputs.txt created by preprocessing.bash
+# Output: ??
+
+path="."
+epoch=25
+batch=256
+ext=.covBga.txt
+ref_prefix="chr"
+save=0
+overwrite=0
 
 helpFunction()
 {
-	   echo ""
-	   echo "Usage: $0 -p \"path to preprocessing data\" -f \"indiv1 indiv2\""
-	   echo -e "\t-p path to preprocessing data."
-	   echo -e "\t-f name of the replicate file (without suffix)"
-	   echo -e "\t-e number of epoch (default 25)."
-	   echo -e "\t-b batch size (default 256)."
-	   exit 1 # Exit script after printing help
+	echo ""
+	echo "Usage: $0 [-d PATH] -b \"BAM_FILE1 BAM_FILE2[ BAM_FILE3...]\""
+	echo -e "\t-b list of names of preprocessed replicate BAM files, surrounded by double quotes."
+	echo -e "\t   These BAM files should have alread been preprocessed by preprocessing.bash."
+	echo -e "\t   Example (from tutorial): \"MCF7_chr10_rep1.bam MCF7_chr10_rep2.bam\""
+	echo -e "\t-d path to data files (DEFAULT: $path)."
+	echo -e "\t-e number of epochs (DEFAULT: $epoch)."
+	echo -e "\t-r Reference sequence name prefix (DEFAULT: $ref_prefix)."
+	echo -e "\t-s batch size (DEFAULT: $batch)."
+	echo -e "\t-v save output files (DEFAULT: no)."
+	echo -e "\t-w overwrite existing files (DEFAULT: no)."
+	echo -e "\t-x Extension for coverage files (DEFAULT: .covBga.txt)."
+	echo -e "\t   Chromosome j coverage file for replicate m assumed to be"
+	echo -e "\t   PATH/chrj/BASEm_j.covBga.txt, where BASEm is BAM_FILEm (-f) without .bam extension."
+	exit 1 # Exit script after printing help
 }
 
-epoch=25
-batch=256
-
-while getopts p:f:e:b flag
+while getopts wv?b:d:b:e:s:x: flag
 do
-    case "${flag}" in
-        p) path=${OPTARG};;
-        f) fname=${OPTARG};;
-        e) epoch=${OPTARG};;
-        b) batch=${OPTARG};;
-	?) helpFunction ;; # Print helpFunction	
-    esac
+	case "${flag}" in
+		b) fname=${OPTARG};;
+		d) path=${OPTARG};;
+		e) epoch=${OPTARG};;
+		r) ref_prefix=${OPTARG};;
+		s) batch=${OPTARG};;
+		w) overwrite=1;;
+		v) save=1;;
+		x) ext=${OPTARG};;
+		?) helpFunction ;; # Print helpFunction	
+	esac
 done
+
+path=${path%/}	# remove terminal /
 
 echo "Number of epoch ${epoch}"
 
@@ -34,36 +57,50 @@ then
 	   helpFunction
 fi
 
+rep_names=${fname//.bam/}
 
 #for i in $(seq 1 $nreps)
 nreps=0
-for i in $fname
-do
-((nreps++))
-while read j; do
-        [ "$j" = "X" ] && continue
-        [ "$j" = "Y" ] && continue
-        cat $path/chr${j}/*${i}.covBga.txt >> $path/rep$nreps.txt
-done < $path/chrList.txt
-done
-
-echo "Number of reps ${nreps}, start training"
-python main.py --epochs $epoch --batch_size $batch --datapath $path --n_rep $nreps --modelpath $path/rcl.ckpt &> $path/out.err
-echo "Finish training, start writing results (if your data is large, please give more memory)"
-
-while read j; do
-python rcl_score.py --model $path/rcl.ckpt --dpath $path/chr${j}  --preprocess_region $path/bigInputs.txt --id $j --prefix $path
-done < $path/chrList.txt
-
-cat $path/rcl_*bed > $path/rcl.bed
-rm $path/rcl_*bed 
-rm $path/out.err
-
-nreps=0
-for i in $fname
+for rep_name in $rep_names
 do
 	((nreps++))
-	rm  $path/rep$nreps.txt
+	if [ $overwrite -eq 1 -o ! -s "$path"/rep$nreps.txt ]; then
+		rm -f "$path"/rep$nreps.txt
+		while read chr; do
+			[ "$chr" = "X" ] && continue
+			[ "$chr" = "Y" ] && continue
+			cat "$path"/$ref_prefix"$chr"/"$rep_name""$ext" >> "$path"/rep$nreps.txt
+		done < "$path"/chrList.txt
+	fi
 done
+
+if [ $overwrite -eq 1 -o ! -s "$path"/rcl.ckpt ]; then
+	echo "Number of reps ${nreps}, start training"
+	python main.py --epochs $epoch --batch_size $batch --datapath "$path" --n_rep $nreps --modelpath "$path"/rcl.ckpt &> "$path"/out.err
+	echo "Finish training, start writing results (if your data is large, please give more memory)"
+else
+	echo "Using existing \"$path/rcl.ckpt\" file"
+fi
+
+if [ $overwrite -eq 1 -o ! -s "$path"/rcl.bed ]; then
+	while read chr; do
+		python rcl_score.py --model "$path"/rcl.ckpt --dpath "$path"/chr${chr} --names $rep_names --preprocess_region "$path"/bigInputs.txt --id $chr --prefix "$path"
+	done < "$path"/chrList.txt
+	cat "$path"/rcl_*bed > "$path"/rcl.bed
+else
+	echo "Using existing \"$path/rcl.bed\" file"
+fi
+
+if [ $save -eq 0 ]; then
+	rm -f "$path"/rcl_*bed 
+	rm -f "$path"/out.err
+
+	nreps=0
+	for i in $fname
+	do
+		((nreps++))
+		rm -f "$path"/rep$nreps.txt
+	done
+fi
 
 echo "Finished!"
